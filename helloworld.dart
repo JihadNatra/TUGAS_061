a568fc8953a6b3dcca386e608b19832bcebbfea8
JihadNatra <127073178+JihadNatra@users.noreply.github.com>
2023-03-09 17:27:50 +0700

    Add files via upload

---
+++ helloworld.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'Halo Dunia',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Halo Dunia'),
+        ),
+        body: Center(
+          child: Text(
+            'JIHAD NATRA SITUMORANG \n1207070061 \n Kelas B',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+      )));
+}
